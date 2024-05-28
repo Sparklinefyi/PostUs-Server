7b359e9c56818fa90f1a865b70823b16253fd0d1
Jacob Kennedy <iblooze2@gmail.com>
2024-05-27 23:26:26 -0500

    Added database connection

--- src/main/kotlin/postus/Application.kt
+++ src/main/kotlin/postus/Application.kt
@@ -1,5 +1,4 @@
 package postus
-
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -7,8 +6,13 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import postus.endpoints.*
 
+import postus.controllers.SocialController
+import postus.controllers.UserController
+import org.slf4j.LoggerFactory
+import postus.Database
+
 fun main() {
-    embeddedServer(Netty, port = 8080, module = Application::module)
+    embeddedServer(Netty, port = 8080, host="localhost", module = Application::module)
         .start(wait = true)
 }
 
@@ -16,6 +20,10 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+
+    val userController = UserController()
+    userController.getAllUsers()
+
     configureAuthRouting()
     configureMediaRouting()
     configureSocialsRouting()
