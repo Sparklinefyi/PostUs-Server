50ef029150f10dfeefd70537576c01a41e8f021d
Jacob Kennedy <iblooze2@gmail.com>
2024-06-02 01:53:46 -0500

    .

--- src/main/kotlin/postus/dto/TokenResponse.kt
+++ src/main/kotlin/postus/dto/TokenResponse.kt
@@ -4,13 +4,3 @@ data class TokenResponse(
     val accessToken: String,
     val refreshToken: String,
 )
-
-data class UserInfo(
-    val id: Int?,
-    val provider: String,
-    val providerUserId: String,
-    val email: String,
-    val name: String?,
-    val accessToken: String,
-    val refreshToken: String?
-)
